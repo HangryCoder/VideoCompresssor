bf55c323f9ce5171e078e670f734317041ecd699
Sonia <soniawadji@gmail.com>
2020-04-12 18:52:33 +0530

    Displaying only videos on click of select videos.

--- app/src/main/java/com/hangrycoder/videocompressor/MainActivity.kt
+++ app/src/main/java/com/hangrycoder/videocompressor/MainActivity.kt
@@ -2,7 +2,6 @@ package com.hangrycoder.videocompressor
 
 import android.content.Intent
 import android.os.Bundle
-import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,12 +13,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         selectVideoButton.setOnClickListener {
-            startActivity(
-                Intent(
-                    Intent.ACTION_PICK,
-                    MediaStore.Video.Media.EXTERNAL_CONTENT_URI
-                )
-            )
+            val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+                type = "video*/"
+            }
+            startActivity(intent)
         }
     }
 }
