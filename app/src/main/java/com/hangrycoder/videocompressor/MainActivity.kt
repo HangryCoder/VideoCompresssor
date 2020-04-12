bb9b3ad27847f430d985518175761df8038cb4eb
Sonia <soniawadji@gmail.com>
2020-04-12 18:49:00 +0530

    Opening gallery on click of select video button.

--- app/src/main/java/com/hangrycoder/videocompressor/MainActivity.kt
+++ app/src/main/java/com/hangrycoder/videocompressor/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.hangrycoder.videocompressor
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        selectVideoButton.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_PICK,
+                    MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                )
+            )
+        }
     }
 }
