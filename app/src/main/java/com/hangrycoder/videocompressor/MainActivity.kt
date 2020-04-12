95e28ab7589b0676111bc25e60d58056f981f10b
Sonia <soniawadji@gmail.com>
2020-04-12 19:12:42 +0530

    Getting video uri. Fixed video not selectable issue.

--- app/src/main/java/com/hangrycoder/videocompressor/MainActivity.kt
+++ app/src/main/java/com/hangrycoder/videocompressor/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.hangrycoder.videocompressor
 
 import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +20,42 @@ class MainActivity : AppCompatActivity() {
 
         selectVideoButton.setOnClickListener {
             val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
-                type = "video*/"
+                type = "video/*"
             }
-            startActivity(intent)
+            startActivityForResult(
+                Intent.createChooser(intent, SELECT_VIDEO),
+                REQUEST_GALLERY_VIDEO
+            )
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_GALLERY_VIDEO) {
+                val selectedImageUri = data?.data
+                Log.e(TAG, "$selectedImageUri")
+            }
+        }
+    }
+
+    fun getPath(uri: Uri?): String? {
+        val projection =
+            arrayOf(MediaStore.Video.Media.DATA)
+        val cursor: Cursor? = contentResolver.query(uri!!, projection, null, null, null)
+        return if (cursor != null) {
+            // HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
+            // THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
+            val column_index: Int = cursor
+                .getColumnIndexOrThrow(MediaStore.Video.Media.DATA)
+            cursor.moveToFirst()
+            cursor.getString(column_index)
+        } else null
+    }
+
+    companion object {
+        private const val SELECT_VIDEO = "Select Video"
+        private const val REQUEST_GALLERY_VIDEO = 111
     }
 }
