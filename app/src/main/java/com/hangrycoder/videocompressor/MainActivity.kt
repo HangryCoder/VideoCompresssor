f8f7765c0e2e933e660047021aa830cfe0cc3627
Sonia <soniawadji@gmail.com>
2020-04-12 20:47:48 +0530

    Added code to request permission for api level 23 and above.

--- app/src/main/java/com/hangrycoder/videocompressor/MainActivity.kt
+++ app/src/main/java/com/hangrycoder/videocompressor/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.hangrycoder.videocompressor
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.hangrycoder.videocompressor.utils.UriUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,14 +21,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         selectVideoButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
-                type = "video/*"
+            //Check if storage permission is granted
+            if (isStoragePermissionGranted()) {
+                fetchVideoFromGallery()
             }
-            startActivityForResult(
-                Intent.createChooser(intent, SELECT_VIDEO),
-                REQUEST_GALLERY_VIDEO
-            )
         }
+    }
+
+    private fun fetchVideoFromGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "video/*"
+        }
+        startActivityForResult(
+            Intent.createChooser(intent, SELECT_VIDEO),
+            REQUEST_GALLERY_VIDEO
+        )
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -38,6 +49,40 @@ class MainActivity : AppCompatActivity() {
                 Log.e(TAG, "$selectedImagePath")
 
             }
+        }
+    }
+
+    private fun isStoragePermissionGranted(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                Log.v(TAG, "Permission is granted")
+                true
+            } else {
+                Log.v(TAG, "Permission is revoked")
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    1
+                )
+                false
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG, "Permission is granted")
+            true
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.v(TAG, "Permission: " + permissions[0] + " was " + grantResults[0])
+            fetchVideoFromGallery()
         }
     }
 
