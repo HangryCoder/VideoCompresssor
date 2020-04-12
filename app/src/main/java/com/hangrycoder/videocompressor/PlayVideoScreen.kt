3c006d4a645fdcf6d8978ac79c1360c305ddec38
Sonia <soniawadji@gmail.com>
2020-04-12 19:39:16 +0530

    Created a screen with video view.

---
+++ app/src/main/java/com/hangrycoder/videocompressor/PlayVideoScreen.kt
@@ -0,0 +1,12 @@
+package com.hangrycoder.videocompressor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlayVideoScreen: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_video)
+    }
+}
