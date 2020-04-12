87d481fe9913650bb53a15892b3e06e9ffd67a69
Sonia <soniawadji@gmail.com>
2020-04-12 20:14:39 +0530

    Successful in playing a random video on video view.

--- app/src/main/java/com/hangrycoder/videocompressor/PlayVideoScreen.kt
+++ app/src/main/java/com/hangrycoder/videocompressor/PlayVideoScreen.kt
@@ -1,12 +1,25 @@
 package com.hangrycoder.videocompressor
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.MediaController
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_play_video.*
 
-class PlayVideoScreen: AppCompatActivity() {
+class PlayVideoScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_video)
+
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        val uri: Uri = Uri.parse(
+            getExternalFilesDir(null)?.path + "/KrupaUke.mp4"
+        )
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
     }
 }
