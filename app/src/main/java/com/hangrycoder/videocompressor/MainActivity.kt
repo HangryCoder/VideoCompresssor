34cd28de4e35d504decd0ad3774d7318d9707fcb
Sonia <soniawadji@gmail.com>
2020-04-12 21:07:32 +0530

    Requesting multiple permission requests for READ and WRITE EXTERNAL STORAGE.

--- app/src/main/java/com/hangrycoder/videocompressor/MainActivity.kt
+++ app/src/main/java/com/hangrycoder/videocompressor/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.hangrycoder.videocompressor
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -14,16 +14,16 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var TAG = MainActivity::class.java.simpleName
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         selectVideoButton.setOnClickListener {
             //Check if storage permission is granted
-            if (isStoragePermissionGranted()) {
+            if (hasPermissions(this, *PERMISSIONS)) {
                 fetchVideoFromGallery()
+            } else {
+                ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL)
             }
         }
     }
@@ -52,27 +52,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun isStoragePermissionGranted(): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                == PackageManager.PERMISSION_GRANTED
-            ) {
-                Log.v(TAG, "Permission is granted")
-                true
-            } else {
-                Log.v(TAG, "Permission is revoked")
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    1
-                )
-                false
-            }
-        } else {
-            Log.v(TAG, "Permission is granted")
-            true
+    private fun hasPermissions(context: Context, vararg permissions: String): Boolean =
+        permissions.all {
+            ActivityCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
         }
-    }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -80,13 +63,23 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            Log.v(TAG, "Permission: " + permissions[0] + " was " + grantResults[0])
-            fetchVideoFromGallery()
+        if (requestCode == PERMISSION_ALL) {
+            for (i in permissions.indices) {
+                Log.v(TAG, "Permission: " + permissions[i] + " was " + grantResults[i])
+                fetchVideoFromGallery()
+            }
         }
     }
 
     companion object {
+
+        private var TAG = MainActivity::class.java.simpleName
+        private val PERMISSIONS = arrayOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+        private const val PERMISSION_ALL = 1
+
         private const val SELECT_VIDEO = "Select Video"
         private const val REQUEST_GALLERY_VIDEO = 111
     }
