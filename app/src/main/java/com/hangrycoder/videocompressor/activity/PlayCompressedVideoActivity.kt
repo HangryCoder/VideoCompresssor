3faa0d3e709a49824fd55871376089d2cf79bcfd
Sonia <soniawadji@gmail.com>
2020-04-13 10:41:32 +0530

    ExoPlayer seems to be working.

--- app/src/main/java/com/hangrycoder/videocompressor/activity/PlayCompressedVideoActivity.kt
+++ app/src/main/java/com/hangrycoder/videocompressor/activity/PlayCompressedVideoActivity.kt
@@ -1,9 +1,17 @@
 package com.hangrycoder.videocompressor.activity
 
+import android.net.Uri
 import android.os.Bundle
 import android.widget.MediaController
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.DefaultRenderersFactory
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.hangrycoder.videocompressor.R
 import com.hangrycoder.videocompressor.databinding.ActivityPlayCompressedVideoBindingImpl
 import com.hangrycoder.videocompressor.utils.Util
@@ -17,7 +25,8 @@ class PlayCompressedVideoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         initDataBinding()
         setIntentParams()
-        initVideoAndPlay()
+        // initVideoAndPlay()
+        initializePlayer()
     }
 
     private fun initDataBinding() {
@@ -32,21 +41,44 @@ class PlayCompressedVideoActivity : AppCompatActivity() {
         Util.showLogE(TAG, "CompressedVideoPath $videoPath")
     }
 
-    private fun initVideoAndPlay() {
-        val mediaController = MediaController(this)
-        mediaController.setAnchorView(videoView)
-        videoView.setMediaController(mediaController)
-        //This is the make the media controller appear above the video view
-        videoView.setOnPreparedListener {
-            it.setOnVideoSizeChangedListener { _, _, _ ->
-                videoView.setMediaController(mediaController)
-                mediaController.setAnchorView(videoView)
-                mediaController.show()
-            }
-        }
-        videoView.setVideoPath(videoPath)
-        videoView.requestFocus()
-        videoView.start()
+    /*  private fun initVideoAndPlay() {
+          val mediaController = MediaController(this)
+          mediaController.setAnchorView(videoView)
+          videoView.setMediaController(mediaController)
+          //This is the make the media controller appear above the video view
+          videoView.setOnPreparedListener {
+              it.setOnVideoSizeChangedListener { _, _, _ ->
+                  videoView.setMediaController(mediaController)
+                  mediaController.setAnchorView(videoView)
+                  mediaController.show()
+              }
+          }
+          videoView.setVideoPath(videoPath)
+          videoView.requestFocus()
+          videoView.start()
+      }*/
+
+    private fun initializePlayer() {
+
+        val trackSelector = DefaultTrackSelector()
+        val loadControl = DefaultLoadControl()
+        val renderersFactory = DefaultRenderersFactory(this)
+
+        val exoPlayer = ExoPlayerFactory.newSimpleInstance(
+            this,
+            renderersFactory, trackSelector, loadControl
+        )
+
+        val userAgent = "Compressed Video"
+        val mediaSource = ExtractorMediaSource
+            .Factory(DefaultDataSourceFactory(this, userAgent))
+            .setExtractorsFactory(DefaultExtractorsFactory())
+            .createMediaSource(Uri.parse(videoPath))
+
+        exoPlayer.prepare(mediaSource)
+        exoPlayer.playWhenReady = true
+
+        videoView.player = exoPlayer
     }
 
     companion object {
