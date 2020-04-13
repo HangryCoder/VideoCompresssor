15c45b254153fddb81a24edc3ccf9593c1237d50
Sonia <soniawadji@gmail.com>
2020-04-13 12:33:52 +0530

    Cleaned up some code and removed commented code.

--- app/src/main/java/com/hangrycoder/videocompressor/compressvideo/CompressVideoActivity.kt
+++ app/src/main/java/com/hangrycoder/videocompressor/compressvideo/CompressVideoActivity.kt
@@ -12,17 +12,11 @@ import com.hangrycoder.videocompressor.activity.PlayCompressedVideoActivity
 import com.hangrycoder.videocompressor.databinding.ActivityCompressVideoBindingImpl
 import com.hangrycoder.videocompressor.utils.*
 import kotlinx.android.synthetic.main.activity_compress_video.*
-import java.io.File
 
 class CompressVideoActivity : AppCompatActivity(), CompressVideoContract.CompressVideoView {
 
-   // private var simpleMediaPlayer: SimpleMediaPlayer? = null
     private val TAG = CompressVideoActivity::class.java.simpleName
-
     private var videoUri: Uri? = null
-  //  private var compressedVideosFolder: File? = null
-   // private var outputFileAbsolutePath: String? = null
-
     private val progressDialog: ProgressDialog by lazy {
         ProgressDialog(this).apply {
             setMessage("Video compression is in progress. Please wait :)")
@@ -30,38 +24,6 @@ class CompressVideoActivity : AppCompatActivity(), CompressVideoContract.Compres
             setCancelable(false)
         }
     }
-
-  /*  private val videoCompression: VideoCompression by lazy {
-        VideoCompression(this, object : VideoCompression.CompressionCallbacks {
-            override fun onStart() {
-                showLoader()
-            }
-
-            override fun onFinish() {
-                hideLoader()
-            }
-
-            override fun onSuccess() {
-                Util.showToast(applicationContext, "Video compressed successfully")
-                val intent = Intent(
-                    this@CompressVideoActivity,
-                    PlayCompressedVideoActivity::class.java
-                ).apply {
-                    putExtra(
-                        PlayCompressedVideoActivity.INTENT_COMPRESSED_VIDEO_PATH,
-                        outputFileAbsolutePath
-                    )
-                }
-                startActivity(intent)
-                finish()
-            }
-
-            override fun onFailure() {
-                Util.showToast(applicationContext, "Video compression failed")
-            }
-
-        })
-    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,29 +43,11 @@ class CompressVideoActivity : AppCompatActivity(), CompressVideoContract.Compres
             this, R.layout.activity_compress_video
         )
         binding.setCompressVideoClick {
-            createOutputFolderIfDoesntExistAndCompressVideo()
+            compressVideo()
         }
     }
 
-    private fun createOutputFolderIfDoesntExistAndCompressVideo() {
-        /* val inputFilePath = UriUtils.getImageFilePath(this, videoUri)
-         inputFilePath ?: return
-
-         compressedVideosFolder = File(
-             getExternalFilesDir(null)?.path + File.separator.toString() + OUTPUT_FILE_DIRECTORY_NAME
-         )
-         compressedVideosFolder?.createFolderIfDoesntExist()
-
-         outputFileAbsolutePath = compressedVideosFolder?.absolutePath +
-                 File.separator.toString() +
-                 System.currentTimeMillis() + FILE_EXTENTION
-         outputFileAbsolutePath ?: return
-
-         videoCompression.compressVideo(
-             bitrate = inputBitrate.text.toString(),
-             inputFilePath = inputFilePath,
-             outputFilePath = outputFileAbsolutePath
-         )*/
+    private fun compressVideo() {
         compressVideoViewModel.compressVideo(
             videoUri = videoUri,
             bitrate = inputBitrate.text.toString()
@@ -116,9 +60,6 @@ class CompressVideoActivity : AppCompatActivity(), CompressVideoContract.Compres
     }
 
     private fun initializePlayerAndPlayVideo() {
-        /* simpleMediaPlayer = SimpleMediaPlayer(this, videoUri)
-         playerView.player = simpleMediaPlayer?.getPlayer()
-         simpleMediaPlayer?.play()*/
         compressVideoViewModel.initMediaPlayer(videoUri)
         playerView.player = compressVideoViewModel.getPlayer()
         compressVideoViewModel.playVideo()
@@ -158,8 +99,6 @@ class CompressVideoActivity : AppCompatActivity(), CompressVideoContract.Compres
     }
 
     companion object {
-       /* private const val FILE_EXTENTION = ".mp4"
-        const val OUTPUT_FILE_DIRECTORY_NAME = "CompressedVideos"*/
         const val INTENT_VIDEO_URI = "video_uri"
     }
 }
